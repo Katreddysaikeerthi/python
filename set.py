ca3bb17484ef0f913a9a466e1b8a9940bb5fc362
Katreddysaikeerthi <katreddysaikeerthi@gmail.com>
2025-06-27 19:26:10 +0530

    Update set.py

--- set.py
+++ set.py
@@ -1,4 +1,4 @@
-#1.	Create a set with elements from 1 to 5. Add elements 6 and 7 to the set in one line.
+#1.Create a set with elements from 1 to 5. Add elements 6 and 7 to the set in one line.
 s={1,2,3,4,5}
 s.update({6,7})
 print(s)
@@ -6,7 +6,7 @@ print(s)
 #output:
 #     {1, 2, 3, 4, 5, 6, 7}
 
-#2. Given two sets: Find elements that are present in A or B but not both (symmetric difference).
+#2.Given two sets: Find elements that are present in A or B but not both (symmetric difference).
 A = {1, 2, 3, 4, 5}
 B = {4, 5, 6, 7, 8}
 v=A.symmetric_difference(B)
@@ -15,7 +15,7 @@ print(v)
 #output:
 #     {1, 2, 3, 6, 7, 8}
 
-#3.	Remove an element from a set, but avoid error if element doesn't exist.
+#3.Remove an element from a set, but avoid error if element doesn't exist.
 # Find maximum and minimum element from a set {5, 8, 12, 3, 15, 7}.
 s={5,8,12,3,15,7}
 s.discard(11)
@@ -28,7 +28,7 @@ print("minimum",min(s))
 #     maximum 15
 #     minimum 3
 
-#4.	Create a set with the values: 10, 20, 30, 40. Then add the value 50 to the set.
+#4.Create a set with the values: 10, 20, 30, 40. Then add the value 50 to the set.
 s={10,20,30,40}
 s.add(50)
 print(s)
@@ -36,7 +36,7 @@ print(s)
 #output:
 #     {40, 10, 50, 20, 30}
 
-#5.	Remove an element 30 from a set {10, 20, 30, 40, 50} using a set method.
+#5.Remove an element 30 from a set {10, 20, 30, 40, 50} using a set method.
 
 v={10,20,30,40,50}
 v.remove(30)
@@ -45,14 +45,14 @@ print(v)
 #output:
 #     {50, 20, 40, 10}
 
-#6.	Check whether the number 25 exists in the set {15, 20, 25, 30, 35}.
+#6.Check whether the number 25 exists in the set {15, 20, 25, 30, 35}.
 s={15,20,25,30,35}
 print(25 in s)
 
 #output:
 #     True
 
-#7.	Find the union of two sets:
+#7.Find the union of two sets:
 A = {1, 2, 3, 4}
 B = {3, 4, 5, 6}
 s1=A.union(B)   
@@ -61,7 +61,7 @@ print(s1)
 #output:
 #     {1, 2, 3, 4, 5, 6}
 
-#8.	Find the intersection of two sets:
+#8.Find the intersection of two sets:
 A = {10,20,30,40}
 B = {30,40,50,60}
 s1=A.intersection(B)
